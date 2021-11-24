8b00d3b07759b4d459b7eb29302c28f98a3678dd
Sebastian <sebcyde@hotmail.com>
2021-11-24 13:00:41 +0000

    changed text scroll speed and begun changing color scheme to match desktop version

--- src/Components/HomePage/Components/BNS/BNS.js
+++ src/Components/HomePage/Components/BNS/BNS.js
@@ -1,37 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import LoadingScreen from '../../../../App.js';
+import EntryScreen from '../../../EntryScreen/EntryScreen';
 import './BNS.css';
 
 function BNS() {
 	let TrendingCoins = [];
+	const returnedSpan = useRef();
+	const [Scroller, setScroller] = useState([]);
 
 	useEffect(() => {
 		axios
-			.get('https://api.coingecko.com/api/v3/search/trending')
+			.get(
+				'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h'
+			)
 			.then((response) => {
-				console.log(response.data.coins);
-				response.data.coins.map((Coins) => {
-					TrendingCoins.push(Coins);
-				});
+				console.log(response.data);
+				// response.data.map((Coins) => {
+				// 	return TrendingCoins.push(
+				// 		<span className="returnedCoinContainer" ref={returnedSpan}>
+				// 			<h1 className="returnedCoinTitle">{Coins.id}</h1>
+				// 			<h2>{Coins.current_price}</h2>
+				// 		</span>
+				// 	);
+				// });
+
+				console.log(TrendingCoins);
 			});
-		console.log(TrendingCoins);
-		return () => {
-			TrendingCoins.forEach((TrendingCoin) => {
-				return (
-					<div className="returnedCoinContainer">
-                        <h1 className="returnedCoinTitle"></h1>
-                        
-					</div>
-				);
-			});
-		};
-	}, [LoadingScreen]);
+
+		return () => {};
+	}, [EntryScreen]);
+
 	return (
 		<div id="scroll-container">
-			<div id="scroll-text">
-				This is scrolling text lorem etc fdvenbeb ebebeb ebrb retbetbewrfewv
-			</div>
+			<div id="scroll-text">test text</div>
+			<div className="BNSMain">{TrendingCoins}</div>
 		</div>
 	);
 }
